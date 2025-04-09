cb2fc87e2506eacd037ad8d4ae6e735efc303a3e
HPriya33 <priyankahingmire03@gmail.com>
2025-04-09 11:27:09 +0530

    my second commit

--- demofile.java
+++ demofile.java
@@ -1,6 +1,7 @@
-public class demofile{
-    public static void main(String[] args){
+public class demofile {
+    public static void main(String[] args) {
         System.out.println("this my demo project with java!");
-        
+        System.out.println("this is the second steps of the project!");
+
     }
 }
